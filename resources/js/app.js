20002a131a9855676aa46440b9297b58cc79c525
MUHAMMAD GALIH ILHAM <103205649+Gkyerie@users.noreply.github.com>
2025-06-15 03:30:57 +0700

    Delete resources directory

--- resources/js/app.js
+++
@@ -1,6 +0,0 @@
-import './bootstrap';
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import '@fullcalendar/daygrid/main.css';
-import '/node_modules/@fullcalendar/daygrid/main.css';
-
